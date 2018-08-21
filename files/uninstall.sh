8c49776469c22aeeb370664d1e779e6d50c05cb6
smackbanger <smackbanger@gmail.com>
2018-08-22 07:31:05 +1000

    remove legacy versioner files when performing uninstall

--- files/uninstall.sh
+++ files/uninstall.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 rm -f /usr/bin/vs
+rm -f /usr/local/bin/vs # remove legacy
 rm -rf /usr/local/bin/versioner
 rm -rf /etc/versioner
 rm -rf ~/.vault
 
 pip uninstall versioner
+pip uninstall pick # remove legacy
 
 exit
